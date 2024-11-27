0459531b47c0df69f636c7a8cf3ec9f3248bcad6
AsoAfan <aso.ascs2021@uoh.edu.iq>
2024-11-27 23:25:20 +0300

    update readme and controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,9 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Log;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-
-    }
 
     public function show()
     {
@@ -25,19 +20,15 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request)
     {
-        Log::error("jij" . $request->hasFile("profile_image"));
-        Log::info($request->file("profile_image")->path());
-
         if ($request->hasFile("profile_image")) {
             $image = $request->file("profile_image");
             $imageExt = $image->getClientOriginalExtension();
             $uploadName = "{$request->username}_image_" . time() . "." . $imageExt;
-            Log::error("uploading image", [$uploadName]);
             $path = $image->storeAs("users", $uploadName, "public");
         }
 
         $user = User::create([
-            ...$request->except(["profile_image", "password_confirmation"]),
+            ...$request->only(["username", "email", "password"]),
             "image_url" => asset("/storage/" . ($path ?? null))
         ]);
 
